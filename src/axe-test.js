2bf56e8df92281c3512e7df828f6be82587b1bb6
ttsukagoshi <taro.tsukagoshi@gmail.com>
2022-01-27 13:59:53 +0900

    axe-reportsを使わないこととし、出力項目を調整できるよう設定

--- src/axe-test.js
+++ src/axe-test.js
@@ -1,14 +1,44 @@
+/* --------------------------------------------------- */
+// ユーザ設定
+
+// @axe-core/puppeteerのwithTagsで、テスト基準を選択的に設定できる。
+// withTagsの設定オプションの詳細は公式ドキュメントを参照：
+// https://www.deque.com/axe/core-documentation/api-documentation/#user-content-axe-core-tags
+// 元の設定は ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa', 'best-practice']
+const axeCoreTags = ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa'];
+
+// 出力する結果の種類を指定。詳細は公式ドキュメントを参照：
+// https://www.deque.com/axe/core-documentation/api-documentation/#options-parameter
+// inapplicable: 対象外の基準
+// incomplete: 適合しているか判断できなかった基準
+// passes: 問題なしとされた基準
+// violations: 適合していないと判断された基準
+const resultTypes = ['incomplete', 'violations'];
+
+/* --------------------------------------------------- */
+
 const { AxePuppeteer } = require('@axe-core/puppeteer');
 const puppeteer = require('puppeteer');
 
 // テスト結果を日本語で出力するように設定する。
-// const AXE_LOCALE_JA = require('axe-core/locales/ja.json');
+const AXE_LOCALE_JA = require('axe-core/locales/ja.json');
 const config = {
-  // locale: AXE_LOCALE_JA,
+  locale: AXE_LOCALE_JA,
 };
 
-// テスト結果の出力に、axe-reports を使用するように設定する。
-const AxeReports = require('axe-reports');
+// 出力されるテスト結果のフィールド名（ヘッダ行）
+const reportHeader = [
+  'URL',
+  'Rule Type', // rule ID
+  'Result Type', // inapplicable, incomplete, passes, or violations
+  'Rule Set', // wcag2aa etc.
+  'Impact', // "minor", "moderate", "serious", or "critical"
+  'Message',
+  'HTML Element',
+  'DOM Element',
+  'Help',
+  'Help URL',
+];
 
 // テスト対象の URL を、外部テキストファイルから読み込んで、配列に整形する。
 const fs = require('fs');
@@ -20,11 +50,15 @@ urls_list = urls_list.split(',');
   const urls = urls_list;
 
   // axe-reports で、見出し行をまずは作成する。
-  AxeReports.createCsvReportHeaderRow();
+  // AxeReports.createCsvReportHeaderRow();
 
   const browser = await puppeteer.launch();
 
   for (let i = 0; i < urls.length; i++) {
+    if (i === 0) {
+      // 出力1行目としてヘッダ行を追加
+      console.log(reportHeader.join());
+    }
     const url = urls[i];
     const page = await browser.newPage();
     await page.setBypassCSP(true);
@@ -39,15 +73,43 @@ urls_list = urls_list.split(',');
       page.goto(`${url}`),
     ]);
 
-    // テストを実行する。withTags で、テスト基準をいろいろ設定できる。
-    // withTagsの設定オプションの詳細は公式ドキュメントを参照: https://www.deque.com/axe/core-documentation/api-documentation/#user-content-axe-core-tags
+    // テストを実行
     const results = await new AxePuppeteer(page)
       .configure(config)
-      .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']) // Originally .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa', 'best-practice'])
+      .withTags(axeCoreTags)
       .analyze();
 
-    // axe-reports で、検証結果の行を追加する。
-    AxeReports.createCsvReportRow(results);
+    // テスト結果をCSVとして出力できるように整形
+    resultTypes.forEach((resultType) => {
+      results[resultType].forEach((resultItem) => {
+        resultItem.nodes.forEach((node) => {
+          node.any.forEach((a) => {
+            console.log(
+              [
+                results.url,
+                resultItem.id,
+                resultType,
+                resultItem.tags
+                  .filter((tag) => axeCoreTags.includes(tag))
+                  .join(),
+                resultItem.impact,
+                a.message,
+                node.html,
+                node.target.join(),
+                resultItem.help,
+                resultItem.helpUrl,
+              ]
+                .map((value) =>
+                  String(value)
+                    .replace(/,/g, '-')
+                    .replace(/(\n|\r|\r\n)/gm, ' ')
+                )
+                .join()
+            );
+          });
+        });
+      });
+    });
 
     await page.close();
   }
